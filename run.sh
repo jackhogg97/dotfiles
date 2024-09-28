80d62879be6271ffe726ac3b5daba3fabfbd238c
jackhogg97 <jackhogg1997@gmail.com>
2024-09-28 12:13:36 +0100

    Add script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! which stow > /dev/null; then
+ echo "Please install stow"
+ exit 1
+fi
+
+stow zsh
+stow tmux
+stow nvim
+stow git
+
+echo "In Tmux, run '<prefix> I' reload with '<prefix> R'"
+echo "In Neovim, run :PackerSync"
+
+exec zsh
