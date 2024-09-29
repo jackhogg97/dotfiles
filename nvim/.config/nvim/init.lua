b1a6e9f228d6953ed55cfd340e74c0b396712a6e
jackhogg97 <jackhogg1997@gmail.com>
2024-09-29 09:30:44 +0100

    Add bindings for tmux navigator

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,3 +5,15 @@ require('remap')
 require('hardline').setup {
   bufferline = true
 }
+
+require'nvim-tmux-navigation'.setup {
+    disable_when_zoomed = true,
+    keybindings = {
+      left = "<C-h>",
+      down = "<C-j>",
+      up = "<C-k>",
+      right = "<C-l>",
+      last_active = "<C-\\>",
+      next = "<C-Space>",
+    }
+}
