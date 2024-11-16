2dd5fe08a40f2382877499dee3718c9f51aca10e
jackhogg97 <jackhogg1997@gmail.com>
2024-11-16 17:55:15 +0000

    Remove unused plugins from lsp

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -27,12 +27,10 @@ return require('packer').startup(function(use)
       {'hrsh7th/cmp-nvim-lsp'},     -- Required
       {'hrsh7th/cmp-buffer'},       -- Optional
       {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
       {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
       -- Snippets
       {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
     }
   }
 
