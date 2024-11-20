9e3da7a5baf793cf0052de5b777cb6f5149e53f9
jackhogg97 <jackhogg1997@gmail.com>
2024-11-20 08:22:39 +0000

    Replace packer with lazy

--- nvim/.config/nvim/after/plugin/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
-vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
