db85b4f09f02fae651bafc727aaeb08dab5c0058
jackhoggbbc <jack.hogg@bbc.co.uk>
2026-01-16 10:28:42 +0000

    Add fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,8 +2,7 @@ return {
   -- Git plugin
   -- https://github.com/tpope/vim-fugitive
   "tpope/vim-fugitive",
-  -- cond = function()
-  --   local git_dir = vim.fn.finddir(".git", vim.fn.getcwd() .. ";")
-  --   return git_dir ~= ""
-  -- end,
+  dependencies = {
+    "tpope/vim-rhubarb", -- Enables GBrowse
+  },
 }
